de22dd975d043599f5ff8351841dbe1af93ca998
weekband <71737893+weekband@users.noreply.github.com>
2021-09-15 20:07:43 +0900

    conflict test

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -1,7 +1,7 @@
 package test.http;
 
 public class GitTest {
-	private static final String DESC = "local GIT Test용 클래스 입니다.";
-	private static final String DEV = "dev브랜치 테스트 시작";
-	private static final String DEV_FIN = "DEV 브렌치 테스트 종료";
+	private static final String DESC = "[git hub]local GIT Test용 클래스 입니다.";
+	private static final String DEV = "[git hub]dev브랜치 테스트 시작";
+	private static final String DEV_FIN = "[git hub]DEV 브렌치 테스트 종료";
 }
