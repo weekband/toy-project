f07d3c881eba93f002636a2b900efdbb8ec36fed
weekband <71737893+weekband@users.noreply.github.com>
2021-09-15 20:25:51 +0900

    conflict 연습

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -1,9 +1,10 @@
 package test.http;
 
 public class GitTest {
-	private static final String DESC = "local GIT Test용 클래스 입니다.";
+	
+	private static final String DESC = "[gihub]local GIT Test용 클래스 입니다.";
 	private static final String DEV = "dev브랜치 테스트 시작";
-	private static final String DEV_FIN = "DEV 브렌치 테스트 종료";
+	private static final String DEV_FIN = "[gihub]DEV 브렌치 테스트 종료";
 	private static final String CONFLICT = "Conflict 태스트";
-	private static final String MERGETOOL = "merge tool 사용";
+	private static final String MERGETOOL = "[gihub]merge tool 사용";
 }
