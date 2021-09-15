1b073cb515d12d0c214e674a890ccec3c471fe9c
weekband <gyqja15@gmail.com>
2021-09-15 20:24:31 +0900

    [문효범] conflict 준비

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -1,7 +1,9 @@
 package test.http;
 
 public class GitTest {
-	private static final String DESC = "[git hub]local GIT Test용 클래스 입니다.";
-	private static final String DEV = "dev브랜치 테스트 시작[local]";
-	private static final String DEV_FIN = "[git hub]DEV 브렌치 테스트 종료";
+	private static final String DESC = "local GIT Test용 클래스 입니다.";
+	private static final String DEV = "dev브랜치 테스트 시작";
+	private static final String DEV_FIN = "DEV 브렌치 테스트 종료";
+	private static final String CONFLICT = "Conflict 태스트";
+	private static final String MERGETOOL = "merge tool 사용";
 }
