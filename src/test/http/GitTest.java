970a5e55eb0cbc98cb053e2c25a2f97d8e6ddedd
weekband <gyqja15@gmail.com>
2021-09-15 19:55:07 +0900

    [문효범] dev 브랜치 테스트 종료

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -2,4 +2,6 @@ package test.http;
 
 public class GitTest {
 	private static final String DESC = "local GIT Test용 클래스 입니다.";
+	private static final String DEV = "dev브랜치 테스트 시작";
+	private static final String DEV_FIN = "DEV 브렌치 테스트 종료";
 }
